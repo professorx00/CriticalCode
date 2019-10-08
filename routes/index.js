0065e6f8c6c275658ca8ea578eb8075600e2a420
Elias Roussos <eliasisaiah@outlook.com>
2019-10-07 20:55:18 -0400

    -deleted commented out code

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ const htmlRoutes = require('./htmlRoutes.js');
 routes.use('/api', apiRoutes);
 routes.use('/', htmlRoutes);
 
-// routes.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Connected!' });
-// });
-
 module.exports = routes;
