89119c8e9fa655bd9a31bbc6466cb23ebc48f179
Markus Markowich <markus902@gmx.de>
2019-10-08 20:31:28 -0400

    latest update from group

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,8 @@
-var db = require("../models");
+let routes = require("express").Router();
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+routes.get("/", (req, res) => {
+  console.log(req.body);
+  res.send("hello GET API");
+});
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+module.exports = routes;
