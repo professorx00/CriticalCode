db8671180ea108e34856285a56409b50012b65c8
Dave Ward <professorx00@gmail.com>
2019-10-10 16:28:56 -0400

    Calculator.JS that will allow for calculations on stats when we need it

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 var db = require("../models");
-
+let calculator = require("./calculator.js");
 let routes = require("express").Router();
 
 routes.get("/", (req, res) => {
@@ -218,6 +218,16 @@ routes.post("/addCharacter", (req, res) => {
       console.log("err", err);
       throw err;
     });
+});
+
+routes.post("/cal/", (req, res) => {
+  console.log(req.body);
+  console.log(calculator.abiityMod(3));
+  console.log(calculator.abiityMod(22));
+  console.log(calculator.abiityMod(13));
+  res.json({
+    data: "Here is where an array of data will go after calculate it"
+  });
 });
 
 module.exports = routes;
