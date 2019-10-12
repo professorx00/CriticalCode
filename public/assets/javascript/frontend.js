036fbed95ab43ef53895b6e82eecf820b2ef16b7
Markus Markowich <markus902@gmx.de>
2019-10-12 13:30:33 -0400

    added ajax calls to frontend

--- public/assets/javascript/frontend.js
+++ public/assets/javascript/frontend.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {
 
-    // on click event for button
+    let spells = $.ajax({
+            url: "/api/spells",
+            method: "GET",
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+    let classes = $.ajax({
+            url: "/api/classes",
+            method: "GET",
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+    let skills = $.ajax({
+            url: "/api/skills",
+            method: "GET",
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+    let armor = $.ajax({
+            url: "/api/armor",
+            method: "GET",
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+    $.when(spells, classes, skills, armor).done(function (resSpells, resClasses, resSkills, resArmor) {
+
+        console.log(resSpells);
+        console.log(resClasses);
+        console.log(resSkills);
+        console.log(resArmor);
+    });
 
 });
