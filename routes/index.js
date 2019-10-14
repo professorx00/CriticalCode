10eb21b55b9ea1f4526a364d605948e63d99d110
Dave Ward <professorx00@gmail.com>
2019-10-14 06:40:06 -0400

    updated logo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { ensureAuthenticated } = require("../config/auth");
 const db = require("../models");
 //welcome Page
 router.get("/", (req, res) => {
-  res.render("welcome", {});
+  res.render("login", {});
 });
 
 //Dashboard
