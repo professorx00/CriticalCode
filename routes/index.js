86bc88ed50ce7ffc36c48423eb5d378a92d19fe2
Dave Ward <professorx00@gmail.com>
2019-10-17 22:05:45 -0400

    Welcome was changed  and 404 was added

--- routes/index.js
+++ routes/index.js
@@ -221,4 +221,24 @@ router.post("/photoUpload", (req, res) => {
   photoUpload(req, res);
 });
 
+// router.use(function(req, res, next){
+//   res.status(404);
+
+//   // respond with html page
+//   if (req.accepts('html')) {
+//     res.render('404', { url: req.url });
+//     return;
+//   }
+
+//   // respond with json
+//   if (req.accepts('json')) {
+//     res.send({ error: 'Not found' });
+//     return;
+//   }
+
+//   // default to plain-text. send()
+//   res.type('txt').send('Not found');
+// });
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -66,6 +66,26 @@ app.use("/api", require("./routes/apiRoutes"));
 
 // Turn on that server!
 
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 db.sequelize.sync().then(function() {
   app.listen(PORT, () => {
     console.log("App listening on port 3000");
